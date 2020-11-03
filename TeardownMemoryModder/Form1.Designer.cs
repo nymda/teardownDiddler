8c0c1c7abc4634f59a9ed032eb90036f5ce4d515
jake hartley <puffleboy101@hotmail.co.uk>
2020-11-03 20:07:59 +0000

    commit

---
+++ TeardownMemoryModder/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace TeardownMemoryModder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLoadPos = new System.Windows.Forms.Button();
+            this.btnSavePos = new System.Windows.Forms.Button();
+            this.sPosZ = new System.Windows.Forms.Label();
+            this.sPosY = new System.Windows.Forms.Label();
+            this.sPosX = new System.Windows.Forms.Label();
+            this.cPosZ = new System.Windows.Forms.Label();
+            this.cPosY = new System.Windows.Forms.Label();
+            this.cPosX = new System.Windows.Forms.Label();
+            this.updateCurrentPositions = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbDeleteBoundaries = new System.Windows.Forms.CheckBox();
+            this.setColour = new System.Windows.Forms.ColorDialog();
+            this.cbJetpack = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLoadPos);
+            this.groupBox1.Controls.Add(this.btnSavePos);
+            this.groupBox1.Controls.Add(this.sPosZ);
+            this.groupBox1.Controls.Add(this.sPosY);
+            this.groupBox1.Controls.Add(this.sPosX);
+            this.groupBox1.Controls.Add(this.cPosZ);
+            this.groupBox1.Controls.Add(this.cPosY);
+            this.groupBox1.Controls.Add(this.cPosX);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(346, 120);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Player position";
+            // 
+            // btnLoadPos
+            // 
+            this.btnLoadPos.Location = new System.Drawing.Point(243, 70);
+            this.btnLoadPos.Name = "btnLoadPos";
+            this.btnLoadPos.Size = new System.Drawing.Size(97, 23);
+            this.btnLoadPos.TabIndex = 7;
+            this.btnLoadPos.Text = "Load position (K)";
+            this.btnLoadPos.UseVisualStyleBackColor = true;
+            this.btnLoadPos.Click += new System.EventHandler(this.btnLoadPos_Click);
+            // 
+            // btnSavePos
+            // 
+            this.btnSavePos.Location = new System.Drawing.Point(243, 16);
+            this.btnSavePos.Name = "btnSavePos";
+            this.btnSavePos.Size = new System.Drawing.Size(97, 23);
+            this.btnSavePos.TabIndex = 1;
+            this.btnSavePos.Text = "Save position (I)";
+            this.btnSavePos.UseVisualStyleBackColor = true;
+            this.btnSavePos.Click += new System.EventHandler(this.btnSavePos_Click);
+            // 
+            // sPosZ
+            // 
+            this.sPosZ.AutoSize = true;
+            this.sPosZ.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sPosZ.Location = new System.Drawing.Point(6, 100);
+            this.sPosZ.Name = "sPosZ";
+            this.sPosZ.Size = new System.Drawing.Size(96, 11);
+            this.sPosZ.TabIndex = 6;
+            this.sPosZ.Text = "Saved Z: 0.00";
+            // 
+            // sPosY
+            // 
+            this.sPosY.AutoSize = true;
+            this.sPosY.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sPosY.Location = new System.Drawing.Point(6, 85);
+            this.sPosY.Name = "sPosY";
+            this.sPosY.Size = new System.Drawing.Size(96, 11);
+            this.sPosY.TabIndex = 5;
+            this.sPosY.Text = "Saved Y: 0.00";
+            // 
+            // sPosX
+            // 
+            this.sPosX.AutoSize = true;
+            this.sPosX.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sPosX.Location = new System.Drawing.Point(6, 70);
+            this.sPosX.Name = "sPosX";
+            this.sPosX.Size = new System.Drawing.Size(96, 11);
+            this.sPosX.TabIndex = 4;
+            this.sPosX.Text = "Saved X: 0.00";
+            // 
+            // cPosZ
+            // 
+            this.cPosZ.AutoSize = true;
+            this.cPosZ.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cPosZ.Location = new System.Drawing.Point(6, 46);
+            this.cPosZ.Name = "cPosZ";
+            this.cPosZ.Size = new System.Drawing.Size(110, 11);
+            this.cPosZ.TabIndex = 3;
+            this.cPosZ.Text = "Current Z: 0.00";
+            // 
+            // cPosY
+            // 
+            this.cPosY.AutoSize = true;
+            this.cPosY.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cPosY.Location = new System.Drawing.Point(6, 31);
+            this.cPosY.Name = "cPosY";
+            this.cPosY.Size = new System.Drawing.Size(110, 11);
+            this.cPosY.TabIndex = 2;
+            this.cPosY.Text = "Current Y: 0.00";
+            // 
+            // cPosX
+            // 
+            this.cPosX.AutoSize = true;
+            this.cPosX.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cPosX.Location = new System.Drawing.Point(6, 16);
+            this.cPosX.Name = "cPosX";
+            this.cPosX.Size = new System.Drawing.Size(110, 11);
+            this.cPosX.TabIndex = 1;
+            this.cPosX.Text = "Current X: 0.00";
+            // 
+            // updateCurrentPositions
+            // 
+            this.updateCurrentPositions.Interval = 50;
+            this.updateCurrentPositions.Tick += new System.EventHandler(this.updateCurrentPositions_Tick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbJetpack);
+            this.groupBox2.Controls.Add(this.cbDeleteBoundaries);
+            this.groupBox2.Location = new System.Drawing.Point(12, 138);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(346, 46);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Misc";
+            // 
+            // cbDeleteBoundaries
+            // 
+            this.cbDeleteBoundaries.AutoSize = true;
+            this.cbDeleteBoundaries.Location = new System.Drawing.Point(8, 19);
+            this.cbDeleteBoundaries.Name = "cbDeleteBoundaries";
+            this.cbDeleteBoundaries.Size = new System.Drawing.Size(116, 17);
+            this.cbDeleteBoundaries.TabIndex = 0;
+            this.cbDeleteBoundaries.Text = "Disable boundaries";
+            this.cbDeleteBoundaries.UseVisualStyleBackColor = true;
+            this.cbDeleteBoundaries.CheckedChanged += new System.EventHandler(this.cbDeleteBoundaries_CheckedChanged);
+            // 
+            // cbJetpack
+            // 
+            this.cbJetpack.AutoSize = true;
+            this.cbJetpack.Location = new System.Drawing.Point(130, 19);
+            this.cbJetpack.Name = "cbJetpack";
+            this.cbJetpack.Size = new System.Drawing.Size(64, 17);
+            this.cbJetpack.TabIndex = 1;
+            this.cbJetpack.Text = "Jetpack";
+            this.cbJetpack.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 189);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Teardown diddler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLoadPos;
+        private System.Windows.Forms.Button btnSavePos;
+        private System.Windows.Forms.Label sPosZ;
+        private System.Windows.Forms.Label sPosY;
+        private System.Windows.Forms.Label sPosX;
+        private System.Windows.Forms.Label cPosZ;
+        private System.Windows.Forms.Label cPosY;
+        private System.Windows.Forms.Label cPosX;
+        private System.Windows.Forms.Timer updateCurrentPositions;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox cbDeleteBoundaries;
+        private System.Windows.Forms.ColorDialog setColour;
+        private System.Windows.Forms.CheckBox cbJetpack;
+    }
+}
+
---
+++ TeardownMemoryModder/Form1.cs
@@ -0,0 +1,229 @@
+﻿using Jupiter;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeardownMemoryModder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProcessMemory(int hProcess, Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        internal static extern bool IsWow64Process(SafeProcessHandle processHandle, out bool isWow64Process);
+
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        public List<float> storedPosition = new List<float> { };
+
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private const UInt32 SWP_NOSIZE = 0x0001;
+        private const UInt32 SWP_NOMOVE = 0x0002;
+        private const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+        const int PROCESS_VM_WRITE = 0x0020;
+        const int PROCESS_VM_OPERATION = 0x0008;
+        const int PROCESS_ALL_ACCESS = 0x1F0FFF;
+        Process process;
+        IntPtr handle;
+        Int64 playerInstance;
+        Int64 gameInstance;
+        Int32 processHandle;
+        Int64 sceneInstance;
+
+        float x;
+        float y;
+        float z;
+
+        byte[] origionalWallCode = new byte[8];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+
+            Process[] teardowns = Process.GetProcessesByName("teardown");
+
+            if(teardowns.Length > 0)
+            {
+                process = teardowns[0];
+                handle = OpenProcess(PROCESS_ALL_ACCESS, false, process.Id);
+
+                Byte[] buffer = new Byte[8];
+                Int32 bytesRead = 0;
+                processHandle = (Int32)handle;
+
+                Int64 baseAddress = process.MainModule.BaseAddress.ToInt64() + 0x3E3520;
+                buffer = new byte[8];
+                ReadProcessMemory(processHandle, baseAddress, buffer, buffer.Length, ref bytesRead);
+                gameInstance = BitConverter.ToInt64(buffer, 0);
+
+                buffer = new byte[8];
+                Int64 playerPtr = gameInstance + 0xA0;
+                ReadProcessMemory(processHandle, playerPtr, buffer, buffer.Length, ref bytesRead);
+                playerInstance = BitConverter.ToInt64(buffer, 0);
+
+                buffer = new byte[8];
+                Int64 scenePtr = gameInstance + 0x40;
+                ReadProcessMemory(processHandle, scenePtr, buffer, buffer.Length, ref bytesRead);
+                sceneInstance = BitConverter.ToInt64(buffer, 0);
+
+                int read_vcount = 0;
+                ReadProcessMemory(processHandle, sceneInstance + 0x530, origionalWallCode, origionalWallCode.Length, ref read_vcount);
+
+                updateCurrentPositions.Start();
+            }
+            else
+            {
+                this.Text = "Teardown diddler [TD NOT RUNNING]";
+            }
+            
+        }
+
+        public float setVelo = 3;
+        private void updateCurrentPositions_Tick(object sender, EventArgs e)
+        {
+            Byte[] buffer = new byte[12];
+            int read = 0;
+            ReadProcessMemory(processHandle, playerInstance, buffer, buffer.Length, ref read);
+
+            x = BitConverter.ToSingle(buffer, 0);
+            y = BitConverter.ToSingle(buffer, 4);
+            z = BitConverter.ToSingle(buffer, 8);
+
+            cPosX.Text = "Current X: " + x.ToString("0.00");
+            cPosY.Text = "Current Y: " + y.ToString("0.00");
+            cPosZ.Text = "Current Z: " + z.ToString("0.00");
+
+            short keyStateI = GetAsyncKeyState(0x49);
+            bool isIPressed = ((keyStateI >> 15) & 0x0001) == 0x0001;
+
+            short keyStateK = GetAsyncKeyState(0x4B);
+            bool isKPressed = ((keyStateK >> 15) & 0x0001) == 0x0001;
+
+            short keyStateSpace = GetAsyncKeyState(0x20);
+            bool isSpacePressed = ((keyStateSpace >> 15) & 0x0001) == 0x0001;
+
+            if (isIPressed)
+            {
+                btnSavePos_Click(null, null);
+            }
+            if (isKPressed)
+            {
+                btnLoadPos_Click(null, null);
+            }
+
+            if (isSpacePressed && cbJetpack.Checked)
+            {
+                if(setVelo > 6)
+                {
+                    setVelo = 6;
+                }
+                else
+                {
+                    setVelo += 0.5f;
+                }
+                int wrote = 0;
+                List<byte> nvelocity = new List<Byte> { };
+                nvelocity.AddRange(BitConverter.GetBytes(setVelo));
+                byte[] arrVelocity = nvelocity.ToArray();
+                WriteProcessMemory(processHandle, playerInstance + 0x38+4, arrVelocity, arrVelocity.Length, ref wrote);
+            }
+            else
+            {
+                setVelo = 3;
+            }
+        }
+
+        public IntPtr GetModuleBaseAddress(string processName, string moduleName)
+        {
+            Process process;
+
+            try
+            {
+                process = Process.GetProcessesByName(processName)[0];
+            }
+
+            catch (IndexOutOfRangeException)
+            {
+                throw new ArgumentException($"No process with name {processName} is currently running");
+            }
+            var module = process.Modules.Cast<ProcessModule>().SingleOrDefault(m => string.Equals(m.ModuleName, moduleName, StringComparison.OrdinalIgnoreCase));
+            return module?.BaseAddress ?? IntPtr.Zero;
+        }
+
+        private void btnSavePos_Click(object sender, EventArgs e)
+        {
+            storedPosition = new List<float> { x, y, z };
+            sPosX.Text = "Saved X: " + x.ToString("0.00");
+            sPosY.Text = "Saved Y: " + y.ToString("0.00");
+            sPosZ.Text = "Saved Z: " + z.ToString("0.00");
+        }
+
+        private void btnLoadPos_Click(object sender, EventArgs e)
+        {
+            List<Byte> lBuffer = new List<Byte> { };
+            lBuffer.AddRange(BitConverter.GetBytes(storedPosition[0]));
+            lBuffer.AddRange(BitConverter.GetBytes(storedPosition[1]));
+            lBuffer.AddRange(BitConverter.GetBytes(storedPosition[2]));
+            byte[] buffer = lBuffer.ToArray();
+            int wrote = 0;
+            WriteProcessMemory(processHandle, playerInstance, buffer, buffer.Length, ref wrote);
+        }
+
+        private void cbDeleteBoundaries_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbDeleteBoundaries.Checked)
+            {
+                byte[] noWalls = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+                int wrote = 0;
+                WriteProcessMemory(processHandle, sceneInstance + 0x530, noWalls, noWalls.Length, ref wrote);
+            }
+            else
+            {
+                int wrote = 0;
+                WriteProcessMemory(processHandle, sceneInstance + 0x530, origionalWallCode, origionalWallCode.Length, ref wrote);
+            }
+        }
+
+        private void btnSetFlashlightColour_Click(object sender, EventArgs e)
+        {
+            setColour.FullOpen = true;
+            if(setColour.ShowDialog() == DialogResult.OK)
+            {
+                byte[] buffer = new byte[64];
+
+                int read = 0;
+                ReadProcessMemory(processHandle, sceneInstance + 0xE8, buffer, buffer.Length, ref read);
+            }
+        }
+    }
+}
